a4798cff6d027fa7c7c404520153e92e7c8c52be
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2025-06-18 01:40:12 -0300

    config: add css file to entry in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['./src/index.js', './src/index.css'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
